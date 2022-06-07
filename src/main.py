3faa5f0ab889266da6f0e3f54e9bc8f5750e31c2
Eduardo Dantas <edudaluna2000@gmail.com>
2022-06-07 16:18:39 -0300

    Codigo final com tipos de retornos

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from json import dumps
 
 __trie = {} #arvore dicionario
 
-def insert(word,fromWhere,isWord): 
+def insert(word,fromWhere,isWord) -> bool: 
     if isWord(word,fromWhere)!=False and len(word)!=0: #se nao for palavra vazia e se existir a palavra
         __insertRec(__trie,word,0) #insere a palavra na arvore
         return True
@@ -12,7 +12,7 @@ def insert(word,fromWhere,isWord):
         return False
 
 
-def __insertRec(dictTree,word,index):
+def __insertRec(dictTree,word,index) -> bool:
     current = dictTree #no atual
     if index == len(word): #se chegar no final da palavra
         current['isWord'] = True #marca como palavra
@@ -22,13 +22,13 @@ def __insertRec(dictTree,word,index):
             current[word[index]] = {}
         __insertRec(current[word[index]],word,index+1) #chama recursivamente
 
-def search(word):
+def search(word) -> bool:
     if len(word)==0: #se for palavra vazia retorna falso
         return False
     else:
         return __searchRec(__trie,word,0)
     
-def __searchRec(dictTree,word,index):
+def __searchRec(dictTree,word,index) -> bool:
     current = dictTree #no atual
     if index == len(word): #se chegar no final da palavra
         if 'isWord' in current: #se for palavra retorna verdadeiro
@@ -41,14 +41,14 @@ def __searchRec(dictTree,word,index):
         else:
             return False
     
-def delete(word):
+def delete(word) -> bool:
     if len(word)==0: #se for palavra vazia retorna False
         return False
     else:
         return False if __deleteRec(__trie,word,0) == -1 else True #se retornar -1 retorna False se nao retorna True
          
 
-def __deleteRec(dictTree,word,index):
+def __deleteRec(dictTree,word,index) -> int:
     current = dictTree #no atual
     lenDict = len(current) #numero de nos filhos
     if len(word)==index and 'isWord' in current: #se chegar no final da palavra e for palavra
@@ -64,11 +64,11 @@ def __deleteRec(dictTree,word,index):
         return 1 #retorna que deletou no com tamanho 1
     return 0 # para quebrar o loop quando encontrar primeiro no que faz parte de outra palavra que n seja a deletada
 
-def showAllWords(): #mostra as palavras da arvore
+def showAllWords() -> None: #mostra as palavras da arvore
     print(__generateArray(__trie,"",[]))
     return 
 
-def __generateArray(dictTree,currentString,words):
+def __generateArray(dictTree,currentString,words) -> list:
     current = dictTree #no atual
     if 'isWord' in current.keys(): #se for palavra adiciona a palavra a lista
         words.append(currentString)
@@ -79,23 +79,23 @@ def __generateArray(dictTree,currentString,words):
             words = __generateArray(current[key],currentString+key,words)
     return words
     
-def __insertPrefix(prefix,word):
+def __insertPrefix(prefix,word) -> str:
     return prefix+word
 
-def __iterateList(words,prefix):
+def __iterateList(words,prefix) -> list:
     res = []
     for word in words:
         res.append(__insertPrefix(prefix,word))
     return res
 
-def wordsWith(prefix):
+def wordsWith(prefix) -> list:
     if len(prefix)==0: #se for palavra vazia retorna todas as palavras
         return __generateArray(__trie,"",[])
     else:
         return __iterateList(__getSubTreeAt(__trie,prefix,0),prefix)
 
 
-def __getSubTreeAt(dictTree,word,index):
+def __getSubTreeAt(dictTree,word,index) -> list:
     current = dictTree #no atual
     if index!=len(word) and word[index] not in current:
         return []
@@ -104,10 +104,10 @@ def __getSubTreeAt(dictTree,word,index):
     else:
         return __getSubTreeAt(current[word[index]],word,index+1) #chama recursivamente
 
-def random(): #retorna uma palavra aleatoria
+def random() -> str: #retorna uma palavra aleatoria
     return __randomRec(__trie)
 
-def __randomRec(dictTree):
+def __randomRec(dictTree) -> str:
     current = dictTree  #no atual
     currentChar = choice(list(current.keys())) #pega uma letra aleatoria do no atual
     if currentChar=='isWord': #se for palavra retorna 
@@ -115,9 +115,9 @@ def __randomRec(dictTree):
     else:
         return currentChar+__randomRec(current[currentChar]) #adiciona letra a palavra e chama recursivamente
 
-def __debuggingDict(): # função de debugging que mostra o esquema da arvore formatada
+def __debuggingDict() -> None: # função de debugging que mostra o esquema da arvore formatada
     print(dumps(__trie,indent=4))
     return
 
-def clear():
+def clear() -> None:
     __trie.clear()
